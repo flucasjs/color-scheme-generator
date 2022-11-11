f05c7ff87a245c2da9ea7226675703eababeda33
unknown <flucasjs@gmail.com>
2022-11-10 19:28:42 -0800

    Update animation

--- src/components/ColorForm/ColorForm.js
+++ src/components/ColorForm/ColorForm.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const ColorForm = ({searchScheme, formData, handleChange}) => {
+  const colorInput = React.useRef(null);
+
   return (
     <form onSubmit={searchScheme}>
-      <input className={`color-input`} name="color" type="color" value={formData.color} onChange={handleChange}></input>
+      <motion.div className={`color-input-container`}>
+        <motion.div
+            onClick={() => colorInput.current.click()}
+            className={`color-input-text`}
+          >
+            {formData.color}
+        </motion.div>
+        <input ref={colorInput} className={`color-input`} name="color" type="color" value={formData.color} onChange={handleChange}></input>
+      </motion.div>
+
       <select className={`mode-selector`} name="mode" value={formData.mode} onChange={handleChange}>
         <option value="">--Select a mode--</option>
         <option value="analogic">analogic</option>
