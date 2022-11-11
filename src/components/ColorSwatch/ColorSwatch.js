ca51bd4d46258b218034de29faa729a1682a4b53
unknown <flucasjs@gmail.com>
2022-11-10 19:29:08 -0800

    Update animation

--- src/components/ColorSwatch/ColorSwatch.js
+++ src/components/ColorSwatch/ColorSwatch.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
-const ColorSwatch = ({schemeColor, copyToClipboard, refs, i, isSmall}) => {
+const ColorSwatch = ({schemeColor, copyToClipboard, i, isDesktop}) => {
   const [copied, setCopied] = React.useState(false);
 
   const containerVariants = {
@@ -10,18 +10,27 @@ const ColorSwatch = ({schemeColor, copyToClipboard, refs, i, isSmall}) => {
     },
     initial: {
       x: "0vw", 
-      transition: { delay: `${(isSmall ? (5 - i) : i) / 10}`, mass: 0.4}
+      transition: { 
+        delay: `${(isDesktop ? (5 - i) : i) / 10}`, 
+        mass: 0.4
+      }
     },
     exit: {
       x: "100vw", 
-      transition: { delay: `${(isSmall ? (5 - i) : i) / 10}`, mass: 0.4}
+      transition: { 
+        delay: `${(isDesktop ? (5 - i) : i) / 10}`, 
+        mass: 0.4
+      }
     },
-    hover: isSmall ? { 
-      y: -10, zIndex: 1, 
-      color: '#f8e112', 
-      borderRadius: "10px" 
+    hover: isDesktop ? { 
+      y: -10,
+      zIndex: 1,
+      color: '#f8e112',
+      borderRadius: "10px"
     } : { 
-      y: 0, zIndex: 1 
+      y: 5,
+      scaleY: 1.1,
+      zIndex: 1
     }
   }
 
@@ -37,33 +46,33 @@ const ColorSwatch = ({schemeColor, copyToClipboard, refs, i, isSmall}) => {
         className={'color-container'} 
         style={{'--bg-color': `#${schemeColor}`}} 
         onClick={(e) => {
-          copyToClipboard(i, e);
+          copyToClipboard(schemeColor, e);
           setCopied(true);
           setTimeout(() => {
             setCopied(false)
           }, 1000);
         }}
       >   
-          <motion.span 
-            ref={refs.current[i]} 
-            className={`color-text`} 
-            whileHover={{ color: '#f8e112'}}
-            transition={{ type: "spring", stiffness: 300 }}
-          >
-            {schemeColor}</motion.span>
-            <AnimatePresence>
-            {
-              copied && <motion.span 
-                className={`copied-text`} 
-                initial={{opacity: 0}} 
-                animate={{opacity: 1}}
-                exit={{opacity: 0}}
-              >
-                Copied!
-              </motion.span>
-            }
-            </AnimatePresence>
-        </motion.div>
+        <motion.span 
+          className={`color-text`} 
+          whileHover={{ color: '#f8e112'}}
+          transition={{ type: "spring", stiffness: 300 }}
+        >
+          {schemeColor}
+        </motion.span>
+        <AnimatePresence>
+          {
+            copied && <motion.span 
+              className={`copied-text`} 
+              initial={{opacity: 0}} 
+              animate={{opacity: 1}}
+              exit={{opacity: 0}}
+            >
+              Copied!
+            </motion.span>
+          }
+        </AnimatePresence>
+      </motion.div>
     </AnimatePresence>
   )
 }
