41ce4f2b9c139fe566cea988d7dad62322604202
unknown <flucasjs@gmail.com>
2022-11-10 19:27:42 -0800

    Add custom hook for desktop design

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
 import React from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
 import ColorSwatch from './components/ColorSwatch';
 import ColorForm from './components/ColorForm';
-import { useIsDekstop } from './hooks/utils';
+import { useIsDesktop } from './hooks/utils';
 
 function App() {
-  const isSmall = useIsDekstop();
+  const isDesktop = useIsDesktop();
   const [scheme, setScheme] = React.useState("");
   const [formData, setFormData] = React.useState({
     mode: "analogic",
     color: "#24B1E0"
   })
-  
-  const boolArray = [];
-  const refArray = [];
-  for (let i = 0; i <= 5; i++) {
-    boolArray.push(false);
-    refArray.push(React.createRef())
-  }
-  const [isCopied, setIsCopied] = React.useState(boolArray);
-  const refs = React.useRef(refArray);
-
 
   const searchScheme = async (e) => {
     e && e.preventDefault();
@@ -46,21 +35,14 @@ function App() {
     }))
   }
 
-  // Remove refs.
-  const copyToClipboard = (index, e) => {
-    const text = refs.current[index].current.textContent;
-    setIsCopied(prevState => prevState.map(() => false));
-
-    // https://developer.mozilla.org/en-US/docs/Web/API/Permissions#browser_support
+  const copyToClipboard = (text, e) => {
     navigator.permissions.query({ name: "clipboard-write" }).then(result => {
       if (result.state === "granted" || result.state === "prompt") {
         navigator.clipboard.writeText(text);
-        setIsCopied(prevArray => prevArray.map((bool, i) => (index === i)))
       }
     }).catch(err => {
       if (err.name === "TypeError") {
         navigator.clipboard.writeText(text);
-        setIsCopied(prevArray => prevArray.map((bool, i) => (index === i)))
       }
     });
   }
@@ -77,20 +59,10 @@ function App() {
             {
               scheme &&
               scheme.map((schemeColor, i) => (
-                <ColorSwatch schemeColor={schemeColor} copyToClipboard={copyToClipboard} refs={refs} i={i} isSmall={isSmall}/>
+                <ColorSwatch schemeColor={schemeColor} copyToClipboard={copyToClipboard} i={i} isDesktop={isDesktop}/>
               ))
             }
           </div>
-
-      {/* <div className={`hex-values-container`}>
-        {
-          scheme && scheme.map((schemeColor, i) => (
-            <div key={schemeColor} className={`hex-value-container`}>
-              <span ref={refs.current[i]} onClick={(e) => copyToClipboard(i, e)}>#{schemeColor}</span>
-            </div>
-          ))
-        }
-      </div> */}
     </div>
   );
 }
